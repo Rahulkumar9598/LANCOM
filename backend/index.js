e90bb10f8efe703a7d767babb97bae6f87578c12
rahul2026km-maker <rahul2026km@gmail.com>
2026-04-18 09:45:53 +0530

    changes on index.js

--- backend/index.js
+++ backend/index.js
@@ -59,11 +59,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.options("*", cors());
 
 // Debug middleware (optional - to see what's coming in)
 app.use((req, res, next) => {
